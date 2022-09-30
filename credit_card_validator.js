df9c3f4b089b3618fca7813fe12c899b20f6b094
Somesh-Karpate <karpatesomesh09@gmail.com>
2022-09-30 13:42:33 +0530

    creditcardvalidator

--- credit_card_validator.js
+++ credit_card_validator.js
@@ -1,15 +1,21 @@
 function validateCreditCard(card){
+
+    // TO remove the " - " from the card as we all know the position of it
     var card1 = card.slice(0,4);
     var card2 = card.slice(5,9);
     var card3 = card.slice(10,14);
     var card4 = card.slice(15);
     var CreditCard = card1+card2+card3+card4;
+
+    //check whether the length is 16 or not 
     if(CreditCard.length==16){
-        var num = Number(CreditCard);
-        if(isNaN(num)){
+        var num = Number(CreditCard);       // convert ot number
+        if(isNaN(num)){                       // check whether it's a number or not
             console.log("valid: "+false+", number: "+card+", error: Not a number");
         }else{
-            function allcharactersame(s){   
+
+            // to check whether the characters are same
+            function allcharactersame(s){     
             for(let i=1;i<s.length;i++)   
                 if(s[i] != s[0])
                     return false;
@@ -21,6 +27,9 @@ function validateCreditCard(card){
             console.log("valid: "+false+", number: "+card+", error: All characters are same");
             
         }else{
+            
+            // after we got to know that it is not same we will check if there sum is greater than 16 and if the last character 
+            // is even or not
             var sum=0;
             for(var j = 0;j<CreditCard.length;j++){
                 sum += Number(CreditCard[j]);
