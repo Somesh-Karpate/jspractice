01a3d9530454e584b88672babe8b218d6ffe1f96
Somesh-Karpate <karpatesomesh09@gmail.com>
2022-10-06 19:33:57 +0530

    randomgame

--- randomgame.js
+++ randomgame.js
@@ -1,3 +1,11 @@
+// Write a function called randomGame that selects a random number between 0 and 1 every 1000 
+// milliseconds and each time that a random number is picked, add 1 to a counter. 
+// If the number is greater than .45, stop the timer and console.log the number of tries 
+// it took before we found a number greater than .45.
+
+
+
+
 function randomGame(){
     let num;
     let times = 0;
